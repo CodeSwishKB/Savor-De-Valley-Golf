0cff74171652abf7e59347c7e99af4be3850b080
ruffa000 <131947582+ruffa000@users.noreply.github.com>
2023-04-27 15:28:16 +0800

    Update script.js
    pa edit po sa desserts

--- script.js
+++ script.js
@@ -266,6 +266,154 @@ const foods = [{
     description: ""
 },]
 
-const desserts = [{
-    
-}]
+const dessert = [{
+    itemName: "Mango Float",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Yema Cake",
+    subCategory: "Cake",
+    size: "Half slice",
+    price: "145",
+    description: ""
+}, {
+    itemName: "Yema Cake",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "290",
+    description: ""
+}, {
+    itemName: "Brazo De Mercedes",
+    subCategory: "Cake",
+    size: "Half slice",
+    price: "115",
+    description: ""
+}, {
+    itemName: "Brazo De Mercedes",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "230",
+    description: ""
+}, {
+    itemName: "Bibingka",
+    subCategory: "Hot",
+    size: "Regular",
+    price: "65",
+    description: ""
+}, {
+    itemName: "Leche Flan",
+    subCategory: "Custard",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Buko Salad",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Coffee Jelly",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Buko Pie",
+    subCategory: "Cake",
+    size: "Half slice",
+    price: "95",
+    description: ""
+}, {
+    itemName: "Buko Pie",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "190",
+    description: ""
+}, {
+    itemName: "Halo - Halo",
+    subCategory: "Shaved ice",
+    size: "Small",
+    price: "50",
+    description: ""
+}, {
+    itemName: "Halo - Halo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "65",
+    description: ""
+}, {
+    itemName: "Halo - Halo",
+    subCategory: "Shaved ice",
+    size: "Large",
+    price: "70",
+    description: ""
+},{
+    itemName: "Maja Blanca",
+    subCategory: "Cake",
+    size: "Half",
+    price: "75",
+    description: ""
+}, {
+    itemName: "Maja Blanca",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "150",
+    description: ""
+}, {
+    itemName: "Gulaman",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "30",
+    description: ""
+}, {
+    itemName: "Polvoron",
+    subCategory: "Shortbread",
+    size: "Regular",
+    price: "55",
+    description: ""
+}, {
+    itemName: "Churos",
+    subCategory: "Fried",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Mais Con Hielo",
+    subCategory: "Shaved ice",
+    size: "Small",
+    price: "70",
+    description: ""
+}, {
+    itemName: "Mais Con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "80",
+    description: ""
+}, {
+    itemName: "Mais Con Hielo",
+    subCategory: "Shaved ice",
+    size: "Large",
+    price: "90",
+    description: ""
+}, {
+    itemName: "Saging con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "55",
+    description: ""
+}, {
+    itemName: "Saging con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "60",
+    description: ""
+},{
+    itemName: "Saging con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "65",
+    description: ""
+},]
