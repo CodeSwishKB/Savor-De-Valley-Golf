cd45e5bc5d0077ae3e92d97f7f803385dfd2ad41
ruffa000 <131947582+ruffa000@users.noreply.github.com>
2023-04-27 14:53:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -84,9 +84,273 @@ const drinks = [{
     description: ""
 },  ]
 
-const foods = [{
+const drinks = [{
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+},{
+    itemName: "Bottled Water",
+    subCategory: "Carbonated Drink",
+    size: "Regular",
+    price: 35,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+},  ]
 
-}]
+const foods = [{
+    itemName: "Garlic Bread",
+    subCategory: "Soups",
+    size: "Half Dozen",
+    price: "449.85",
+    description: ""
+}, {
+    itemName: "Chicken Wings",
+    subCategory: "Appetizers",
+    size: "Regular",
+    price: "299.5",
+    description: ""
+}, {
+    itemName: "Chicken Wings",
+    subCategory: "Appetizers",
+    size: "Dozen",
+    price: "749.75",
+    description: ""
+}, {
+    itemName: "Beef Sliders",
+    subCategory: "Appetizers",
+    size: "Single",
+    price: "249.50",
+    description: ""
+}, {
+    itemName: "Beef Sliders",
+    subCategory: "Appetizers",
+    size: "Double",
+    price: "449.85",
+    description: ""
+}, {
+    itemName: "Fried Zucchini  ",
+    subCategory: "Appetizers",
+    size: "Regular",
+    price: "349.65",
+    description: ""
+}, {
+    itemName: "Onion Rings  ",
+    subCategory: "Appetizers",
+    size: "Regular",
+    price: "299.50",
+    description: ""
+}, {
+    itemName: "Jumbo Shrimp Cocktail",
+    subCategory: "Appetizers",
+    size: "Regular",
+    price: "449.85",
+    description: ""
+}, {
+    itemName: "Jumbo Shrimp Cocktail",
+    subCategory: "Appetizers",
+    size: "Large",
+    price: "749.75",
+    description: ""
+}, {
+    itemName: "Mozzarella Sticks",
+    subCategory: "Appetizers",
+    size: " Regular",
+    price: "349.65",
+    description: ""
+}, {
+    itemName: "Tomato Soup",
+    subCategory: "Soups",
+    size: "Cup",
+    price: "179.55",
+    description: ""
+}, {
+    itemName: "Tomato Soup",
+    subCategory: "Soups",
+    size: "Bowl",
+    price: "269.25",
+    description: ""
+}, {
+    itemName: "Chicken Noodle Soup",
+    subCategory: "Soups",
+    size: "Cup",
+    price: "224.65",
+    description: ""
+}, {
+    itemName: "Chicken Noodle Soup",
+    subCategory: "Soups",
+    size: " Bowl",
+    price: "314.35",
+    description: ""
+}, {
+    itemName: "Garden Salad",
+    subCategory: "Salad",
+    size: " Regular",
+    price: "299.50",
+    description: ""
+}, {
+    itemName: "Spaghetti with Meatballs",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "349",
+    description: ""
+}, {
+    itemName: "Classic Filipino Style Spaghetti",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "329",
+    description: ""
+}, {
+    itemName: "Spaghetti with Marinara Sauce",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "329",
+    description: ""
+}, {
+    itemName: "Creamy Shrimp Pasta",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "349",
+    description: ""
+}, {
+    itemName: "Triple Cheese Pasta",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "349",
+    description: ""
+}, {
+    itemName: "Carbonara",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "329",
+    description: ""
+}, {
+    itemName: "Parmesan Cheese Pasta",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "329",
+    description: ""
+}, {
+    itemName: "Veggie Pasta",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "315",
+    description: ""
+}, {
+    itemName: "Veggie Supreme Pizza",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "375",
+    description: ""
+}, {
+    itemName: "BBQ Chicken Pizza",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "375",
+    description: ""
+}, {
+    itemName: "Meat Lovers",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "399",
+    description: ""
+}, {
+    itemName: "Extra Cheesy Pizza",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "399",
+    description: ""
+}, {
+    itemName: "Classic Pepperoni Pizza",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "375",
+    description: ""
+}, {
+    itemName: "Hawaiian Pizza",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "375",
+    description: ""
+}, {
+    itemName: "Chicken & Spinach Pizza",
+    subCategory: "Pasta",
+    size: " Regular",
+    price: "399",
+    description: ""
+},]
 
 const desserts = [{
     
