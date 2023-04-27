0a05267060bbea539922d575278ff426a7531fb3
Kyle Barco <kylebarco)4@gmail.com>
2023-04-27 16:00:53 +0800

    andto na lahat tangina

--- script.js
+++ script.js
@@ -11,78 +11,151 @@ const drinks = [{
     price: 35,
     description: ""
 }, {
-    itemName: "Coke",
+    itemName: "Diet Soda",
     subCategory: "Carbonated Drink",
     size: "Can",
     price: 25,
     description: ""
 }, {
-    itemName: "Coke",
+    itemName: "Lipton",
     subCategory: "Carbonated Drink",
-    size: "Can",
+    size: "Regular",
+    price: 30,
+    description: ""
+}, {
+    itemName: "Ice Tea",
+    subCategory: "Carbonated Drink",
+    size: "Pitcher",
+    price: 55,
+    description: ""
+}, {
+    itemName: "Double Dutch Shake",
+    subCategory: "Shakes",
+    size: "Regular",
+    price: 85,
+    description: ""
+}, {
+    itemName: "Black Forest Shake",
+    subCategory: "Shakes",
+    size: "Regular",
     price: 25,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Banana Shake",
+    subCategory: "Shakes",
+    size: "Small",
+    price: 60,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Banana Shake",
+    subCategory: "Shakes",
+    size: "Medium",
+    price: 110,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Banana Shake",
+    subCategory: "Shakes",
+    size: "Large",
+    price: 180,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Mango Shake",
+    subCategory: "Shakes",
+    size: "Small",
+    price: 60,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Mango Shake",
+    subCategory: "Shakes",
+    size: "Medium",
+    price: 110,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Mango Shake",
+    subCategory: "Shakes",
+    size: "Large",
+    price: 180,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Hot Tea Latte",
+    subCategory: "Hot Coffee",
+    size: "Regular",
+    price: 140,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Cappuccino",
+    subCategory: "Hot Coffee",
+    size: "Regular",
+    price: 140,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Brewed Coffee",
+    subCategory: "Hot Coffee",
+    size: "Regular",
+    price: 140,
     description: ""
 }, {
-    itemName: "Coke",
-    subCategory: "Carbonated Drink",
-    size: "Can",
-    price: 25,
+    itemName: "Mocha Iced Coffee",
+    subCategory: "Cold Coffee",
+    size: "Regular",
+    price: 120,
     description: ""
-},  ]
+}, {
+    itemName: "Vanilla Iced Coffee",
+    subCategory: "Cold Coffee",
+    size: "Regular",
+    price: 120,
+    description: ""
+}, {
+    itemName: "Iced Coffee",
+    subCategory: "Cold Coffee",
+    size: "Regular",
+    price: 120,
+    description: ""
+},{
+    itemName: "Alfonso",
+    subCategory: "Beers",
+    size: "1 Liter",
+    price: 650,
+    description: ""
+},{
+    itemName: "Red Horse",
+    subCategory: "Beers",
+    size: "1 Liter",
+    price: 140,
+    description: ""
+},{
+    itemName: "Red Horse",
+    subCategory: "Bucket",
+    size: "Beers",
+    price: 450,
+    description: ""
+},{
+    itemName: "San Miguel Light",
+    subCategory: "Beers",
+    size: "Solo",
+    price: 70,
+    description: ""
+},{
+    itemName: "San Miguel Light",
+    subCategory: "Beers",
+    size: "Bucket",
+    price: 350,
+    description: ""
+},{
+    itemName: "Pale Pilsen",
+    subCategory: "Beers",
+    size: "Solo",
+    price: 70,
+    description: ""
+},{
+    itemName: "Pale Pilsen",
+    subCategory: "Beers",
+    size: "Bucket",
+    price: 350,
+    description: ""
+} ]
+console.log(drinks.length)
 
 const foods = [{
     itemName: "Garlic Bread",
@@ -266,6 +339,154 @@ const foods = [{
     description: ""
 },]
 
-const desserts = [{
-    
-}]
+const dessert = [{
+    itemName: "Mango Float",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Yema Cake",
+    subCategory: "Cake",
+    size: "Half slice",
+    price: "145",
+    description: ""
+}, {
+    itemName: "Yema Cake",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "290",
+    description: ""
+}, {
+    itemName: "Brazo De Mercedes",
+    subCategory: "Cake",
+    size: "Half slice",
+    price: "115",
+    description: ""
+}, {
+    itemName: "Brazo De Mercedes",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "230",
+    description: ""
+}, {
+    itemName: "Bibingka",
+    subCategory: "Hot",
+    size: "Regular",
+    price: "65",
+    description: ""
+}, {
+    itemName: "Leche Flan",
+    subCategory: "Custard",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Buko Salad",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Coffee Jelly",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Buko Pie",
+    subCategory: "Cake",
+    size: "Half slice",
+    price: "95",
+    description: ""
+}, {
+    itemName: "Buko Pie",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "190",
+    description: ""
+}, {
+    itemName: "Halo - Halo",
+    subCategory: "Shaved ice",
+    size: "Small",
+    price: "50",
+    description: ""
+}, {
+    itemName: "Halo - Halo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "65",
+    description: ""
+}, {
+    itemName: "Halo - Halo",
+    subCategory: "Shaved ice",
+    size: "Large",
+    price: "70",
+    description: ""
+},{
+    itemName: "Maja Blanca",
+    subCategory: "Cake",
+    size: "Half",
+    price: "75",
+    description: ""
+}, {
+    itemName: "Maja Blanca",
+    subCategory: "Cake",
+    size: "Whole",
+    price: "150",
+    description: ""
+}, {
+    itemName: "Gulaman",
+    subCategory: "Cold",
+    size: "Regular",
+    price: "30",
+    description: ""
+}, {
+    itemName: "Polvoron",
+    subCategory: "Shortbread",
+    size: "Regular",
+    price: "55",
+    description: ""
+}, {
+    itemName: "Churos",
+    subCategory: "Fried",
+    size: "Regular",
+    price: "100",
+    description: ""
+}, {
+    itemName: "Mais Con Hielo",
+    subCategory: "Shaved ice",
+    size: "Small",
+    price: "70",
+    description: ""
+}, {
+    itemName: "Mais Con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "80",
+    description: ""
+}, {
+    itemName: "Mais Con Hielo",
+    subCategory: "Shaved ice",
+    size: "Large",
+    price: "90",
+    description: ""
+}, {
+    itemName: "Saging con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "55",
+    description: ""
+}, {
+    itemName: "Saging con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "60",
+    description: ""
+},{
+    itemName: "Saging con Hielo",
+    subCategory: "Shaved ice",
+    size: "Regular",
+    price: "65",
+    description: ""
+},]
