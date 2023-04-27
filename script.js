46c9d125d5c6bc2c669bd3aefddfd4c7fd62098e
Kyle Barco <kylebarco)4@gmail.com>
2023-04-27 14:31:27 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const drinks = [{
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+},{
+    itemName: "Bottled Water",
+    subCategory: "Carbonated Drink",
+    size: "Regular",
+    price: 35,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+}, {
+    itemName: "Coke",
+    subCategory: "Carbonated Drink",
+    size: "Can",
+    price: 25,
+    description: ""
+},  ]
+
+const foods = [{
+
+}]
+
+const desserts = [{
+    
+}]
